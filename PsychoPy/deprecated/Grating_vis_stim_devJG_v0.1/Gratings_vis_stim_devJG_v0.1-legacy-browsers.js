2a62e78c94f91004c59fcd835068859f086cc2e0
Gronemeyer <46824871+Gronemeyer@users.noreply.github.com>
2024-07-12 10:55:36 -0400

    240712_widfield to 2P
    July 12, 2024 version of the grating psychopy replication of the Sipe lab MATLAB vis stim

---
+++ PsychoPy/deprecated/Grating_vis_stim_devJG_v0.1/Gratings_vis_stim_devJG_v0.1-legacy-browsers.js
@@ -0,0 +1,355 @@
+﻿/************************************* 
+ * Gratings_Vis_Stim_Devjg_V0.1 *
+ *************************************/
+
+
+// store info about the experiment session:
+let expName = 'Gratings_vis_stim_devJG_v0.1';  // from the Builder filename that created this script
+let expInfo = {
+    'participant': `${util.pad(Number.parseFloat(util.randint(0, 999999)).toFixed(0), 6)}`,
+    'session': '001',
+};
+
+// Start code blocks for 'Before Experiment'
+// init psychoJS:
+const psychoJS = new PsychoJS({
+  debug: true
+});
+
+// open window:
+psychoJS.openWindow({
+  fullscr: true,
+  color: new util.Color([0,0,0]),
+  units: 'height',
+  waitBlanking: true,
+  backgroundImage: '',
+  backgroundFit: 'none',
+});
+// schedule the experiment:
+psychoJS.schedule(psychoJS.gui.DlgFromDict({
+  dictionary: expInfo,
+  title: expName
+}));
+
+const flowScheduler = new Scheduler(psychoJS);
+const dialogCancelScheduler = new Scheduler(psychoJS);
+psychoJS.scheduleCondition(function() { return (psychoJS.gui.dialogComponent.button === 'OK'); }, flowScheduler, dialogCancelScheduler);
+
+// flowScheduler gets run if the participants presses OK
+flowScheduler.add(updateInfo); // add timeStamp
+flowScheduler.add(experimentInit);
+flowScheduler.add(Daq_TriggerRoutineBegin());
+flowScheduler.add(Daq_TriggerRoutineEachFrame());
+flowScheduler.add(Daq_TriggerRoutineEnd());
+const Sequential_GratingsLoopScheduler = new Scheduler(psychoJS);
+flowScheduler.add(Sequential_GratingsLoopBegin(Sequential_GratingsLoopScheduler));
+flowScheduler.add(Sequential_GratingsLoopScheduler);
+flowScheduler.add(Sequential_GratingsLoopEnd);
+
+
+flowScheduler.add(quitPsychoJS, '', true);
+
+// quit if user presses Cancel in dialog box:
+dialogCancelScheduler.add(quitPsychoJS, '', false);
+
+psychoJS.start({
+  expName: expName,
+  expInfo: expInfo,
+  resources: [
+    // resources:
+  ]
+});
+
+psychoJS.experimentLogger.setLevel(core.Logger.ServerLevel.WARNING);
+
+
+var currentLoop;
+var frameDur;
+async function updateInfo() {
+  currentLoop = psychoJS.experiment;  // right now there are no loops
+  expInfo['date'] = util.MonotonicClock.getDateStr();  // add a simple timestamp
+  expInfo['expName'] = expName;
+  expInfo['psychopyVersion'] = '2024.1.5';
+  expInfo['OS'] = window.navigator.platform;
+
+
+  // store frame rate of monitor if we can measure it successfully
+  expInfo['frameRate'] = psychoJS.window.getActualFrameRate();
+  if (typeof expInfo['frameRate'] !== 'undefined')
+    frameDur = 1.0 / Math.round(expInfo['frameRate']);
+  else
+    frameDur = 1.0 / 60.0; // couldn't get a reliable measure so guess
+
+  // add info from the URL:
+  util.addInfoFromUrl(expInfo);
+  
+
+  
+  psychoJS.experiment.dataFileName = (("." + "/") + `data/${expInfo["participant"]}_${expName}_${expInfo["date"]}`);
+  psychoJS.experiment.field_separator = '\t';
+
+
+  return Scheduler.Event.NEXT;
+}
+
+
+var Daq_TriggerClock;
+var env_gratingClock;
+var globalClock;
+var routineTimer;
+async function experimentInit() {
+  // Initialize components for Routine "Daq_Trigger"
+  Daq_TriggerClock = new util.Clock();
+  // Initialize components for Routine "env_grating"
+  env_gratingClock = new util.Clock();
+  // Create some handy timers
+  globalClock = new util.Clock();  // to track the time since experiment started
+  routineTimer = new util.CountdownTimer();  // to track time remaining of each (non-slip) routine
+  
+  return Scheduler.Event.NEXT;
+}
+
+
+var t;
+var frameN;
+var continueRoutine;
+var Daq_TriggerComponents;
+function Daq_TriggerRoutineBegin(snapshot) {
+  return async function () {
+    TrialHandler.fromSnapshot(snapshot); // ensure that .thisN vals are up to date
+    
+    //--- Prepare to start Routine 'Daq_Trigger' ---
+    t = 0;
+    Daq_TriggerClock.reset(); // clock
+    frameN = -1;
+    continueRoutine = true; // until we're told otherwise
+    // update component parameters for each repeat
+    psychoJS.experiment.addData('Daq_Trigger.started', globalClock.getTime());
+    // keep track of which components have finished
+    Daq_TriggerComponents = [];
+    
+    Daq_TriggerComponents.forEach( function(thisComponent) {
+      if ('status' in thisComponent)
+        thisComponent.status = PsychoJS.Status.NOT_STARTED;
+       });
+    return Scheduler.Event.NEXT;
+  }
+}
+
+
+function Daq_TriggerRoutineEachFrame() {
+  return async function () {
+    //--- Loop for each frame of Routine 'Daq_Trigger' ---
+    // get current time
+    t = Daq_TriggerClock.getTime();
+    frameN = frameN + 1;// number of completed frames (so 0 is the first frame)
+    // update/draw components on each frame
+    // check for quit (typically the Esc key)
+    if (psychoJS.experiment.experimentEnded || psychoJS.eventManager.getKeys({keyList:['escape']}).length > 0) {
+      return quitPsychoJS('The [Escape] key was pressed. Goodbye!', false);
+    }
+    
+    // check if the Routine should terminate
+    if (!continueRoutine) {  // a component has requested a forced-end of Routine
+      return Scheduler.Event.NEXT;
+    }
+    
+    continueRoutine = false;  // reverts to True if at least one component still running
+    Daq_TriggerComponents.forEach( function(thisComponent) {
+      if ('status' in thisComponent && thisComponent.status !== PsychoJS.Status.FINISHED) {
+        continueRoutine = true;
+      }
+    });
+    
+    // refresh the screen if continuing
+    if (continueRoutine) {
+      return Scheduler.Event.FLIP_REPEAT;
+    } else {
+      return Scheduler.Event.NEXT;
+    }
+  };
+}
+
+
+function Daq_TriggerRoutineEnd(snapshot) {
+  return async function () {
+    //--- Ending Routine 'Daq_Trigger' ---
+    Daq_TriggerComponents.forEach( function(thisComponent) {
+      if (typeof thisComponent.setAutoDraw === 'function') {
+        thisComponent.setAutoDraw(false);
+      }
+    });
+    psychoJS.experiment.addData('Daq_Trigger.stopped', globalClock.getTime());
+    // the Routine "Daq_Trigger" was not non-slip safe, so reset the non-slip timer
+    routineTimer.reset();
+    
+    // Routines running outside a loop should always advance the datafile row
+    if (currentLoop === psychoJS.experiment) {
+      psychoJS.experiment.nextEntry(snapshot);
+    }
+    return Scheduler.Event.NEXT;
+  }
+}
+
+
+var Sequential_Gratings;
+function Sequential_GratingsLoopBegin(Sequential_GratingsLoopScheduler, snapshot) {
+  return async function() {
+    TrialHandler.fromSnapshot(snapshot); // update internal variables (.thisN etc) of the loop
+    
+    // set up handler to look after randomisation of conditions etc
+    Sequential_Gratings = new TrialHandler({
+      psychoJS: psychoJS,
+      nReps: 5, method: TrialHandler.Method.SEQUENTIAL,
+      extraInfo: expInfo, originPath: undefined,
+      trialList: undefined,
+      seed: undefined, name: 'Sequential_Gratings'
+    });
+    psychoJS.experiment.addLoop(Sequential_Gratings); // add the loop to the experiment
+    currentLoop = Sequential_Gratings;  // we're now the current loop
+    
+    // Schedule all the trials in the trialList:
+    Sequential_Gratings.forEach(function() {
+      snapshot = Sequential_Gratings.getSnapshot();
+    
+      Sequential_GratingsLoopScheduler.add(importConditions(snapshot));
+      Sequential_GratingsLoopScheduler.add(env_gratingRoutineBegin(snapshot));
+      Sequential_GratingsLoopScheduler.add(env_gratingRoutineEachFrame());
+      Sequential_GratingsLoopScheduler.add(env_gratingRoutineEnd(snapshot));
+      Sequential_GratingsLoopScheduler.add(Sequential_GratingsLoopEndIteration(Sequential_GratingsLoopScheduler, snapshot));
+    });
+    
+    return Scheduler.Event.NEXT;
+  }
+}
+
+
+async function Sequential_GratingsLoopEnd() {
+  // terminate loop
+  psychoJS.experiment.removeLoop(Sequential_Gratings);
+  // update the current loop from the ExperimentHandler
+  if (psychoJS.experiment._unfinishedLoops.length>0)
+    currentLoop = psychoJS.experiment._unfinishedLoops.at(-1);
+  else
+    currentLoop = psychoJS.experiment;  // so we use addData from the experiment
+  return Scheduler.Event.NEXT;
+}
+
+
+function Sequential_GratingsLoopEndIteration(scheduler, snapshot) {
+  // ------Prepare for next entry------
+  return async function () {
+    if (typeof snapshot !== 'undefined') {
+      // ------Check if user ended loop early------
+      if (snapshot.finished) {
+        // Check for and save orphaned data
+        if (psychoJS.experiment.isEntryEmpty()) {
+          psychoJS.experiment.nextEntry(snapshot);
+        }
+        scheduler.stop();
+      } else {
+        psychoJS.experiment.nextEntry(snapshot);
+      }
+    return Scheduler.Event.NEXT;
+    }
+  };
+}
+
+
+var env_gratingComponents;
+function env_gratingRoutineBegin(snapshot) {
+  return async function () {
+    TrialHandler.fromSnapshot(snapshot); // ensure that .thisN vals are up to date
+    
+    //--- Prepare to start Routine 'env_grating' ---
+    t = 0;
+    env_gratingClock.reset(); // clock
+    frameN = -1;
+    continueRoutine = true; // until we're told otherwise
+    // update component parameters for each repeat
+    psychoJS.experiment.addData('env_grating.started', globalClock.getTime());
+    // keep track of which components have finished
+    env_gratingComponents = [];
+    
+    env_gratingComponents.forEach( function(thisComponent) {
+      if ('status' in thisComponent)
+        thisComponent.status = PsychoJS.Status.NOT_STARTED;
+       });
+    return Scheduler.Event.NEXT;
+  }
+}
+
+
+function env_gratingRoutineEachFrame() {
+  return async function () {
+    //--- Loop for each frame of Routine 'env_grating' ---
+    // get current time
+    t = env_gratingClock.getTime();
+    frameN = frameN + 1;// number of completed frames (so 0 is the first frame)
+    // update/draw components on each frame
+    // check for quit (typically the Esc key)
+    if (psychoJS.experiment.experimentEnded || psychoJS.eventManager.getKeys({keyList:['escape']}).length > 0) {
+      return quitPsychoJS('The [Escape] key was pressed. Goodbye!', false);
+    }
+    
+    // check if the Routine should terminate
+    if (!continueRoutine) {  // a component has requested a forced-end of Routine
+      return Scheduler.Event.NEXT;
+    }
+    
+    continueRoutine = false;  // reverts to True if at least one component still running
+    env_gratingComponents.forEach( function(thisComponent) {
+      if ('status' in thisComponent && thisComponent.status !== PsychoJS.Status.FINISHED) {
+        continueRoutine = true;
+      }
+    });
+    
+    // refresh the screen if continuing
+    if (continueRoutine) {
+      return Scheduler.Event.FLIP_REPEAT;
+    } else {
+      return Scheduler.Event.NEXT;
+    }
+  };
+}
+
+
+function env_gratingRoutineEnd(snapshot) {
+  return async function () {
+    //--- Ending Routine 'env_grating' ---
+    env_gratingComponents.forEach( function(thisComponent) {
+      if (typeof thisComponent.setAutoDraw === 'function') {
+        thisComponent.setAutoDraw(false);
+      }
+    });
+    psychoJS.experiment.addData('env_grating.stopped', globalClock.getTime());
+    // the Routine "env_grating" was not non-slip safe, so reset the non-slip timer
+    routineTimer.reset();
+    
+    // Routines running outside a loop should always advance the datafile row
+    if (currentLoop === psychoJS.experiment) {
+      psychoJS.experiment.nextEntry(snapshot);
+    }
+    return Scheduler.Event.NEXT;
+  }
+}
+
+
+function importConditions(currentLoop) {
+  return async function () {
+    psychoJS.importAttributes(currentLoop.getCurrentTrial());
+    return Scheduler.Event.NEXT;
+    };
+}
+
+
+async function quitPsychoJS(message, isCompleted) {
+  // Check for and save orphaned data
+  if (psychoJS.experiment.isEntryEmpty()) {
+    psychoJS.experiment.nextEntry();
+  }
+  psychoJS.window.close();
+  psychoJS.quit({message: message, isCompleted: isCompleted});
+  
+  return Scheduler.Event.QUIT;
+}
